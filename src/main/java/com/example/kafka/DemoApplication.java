ff3d79adfe8def910e75b9678a1854085b14d434
arh91 <alvaro_rh_991@hotmail.com>
2025-04-24 16:45:36 +0200

    añadimos la anotación @EnableKafka a la clase principal que arranca mi servicio Spring, y también unas lineas extra al application.properties

--- src/main/java/com/example/kafka/DemoApplication.java
+++ src/main/java/com/example/kafka/DemoApplication.java
@@ -1,8 +1,10 @@
-package com.example;
+package com.example.kafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
 public class DemoApplication {
 
